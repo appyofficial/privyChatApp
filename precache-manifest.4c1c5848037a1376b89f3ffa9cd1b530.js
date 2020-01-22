c74a082e7a5f3542bae14608866b66ff2149840c
appyofficial <appy70890@gmail.com>
2020-01-22 12:05:20 +0100

    v0.1
    beta

---
+++ precache-manifest.4c1c5848037a1376b89f3ffa9cd1b530.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d823f6355ff79ba5e64c9d24bcdec9d7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fc197945488ec75a33f9",
+    "url": "/static/css/main.8e48434f.chunk.css"
+  },
+  {
+    "revision": "f51b37403fa7124d1371",
+    "url": "/static/js/2.b919dcd0.chunk.js"
+  },
+  {
+    "revision": "fc197945488ec75a33f9",
+    "url": "/static/js/main.1db811e6.chunk.js"
+  },
+  {
+    "revision": "1337678ae3301a2c8a7b",
+    "url": "/static/js/runtime-main.8eac158a.js"
+  },
+  {
+    "revision": "7b5e8e34b274ecc43d7da4d90af7f8da",
+    "url": "/static/media/friendship.7b5e8e34.png"
+  }
+]);
